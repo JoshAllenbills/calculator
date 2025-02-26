4f8c4a1f0c5c19617ddb74b2042a8da40ec5abc3
Colin <colind2824@tuckahoeschools.org>
2025-02-26 13:32:41 +0000

    inital commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//The function below uses the id res to create the solve function of the calculator.//
 function Solve(val) {
    var v = document.getElementById('res');
    v.value += val;
 }
+//The function below uses the id res to create the result function of the calculator.//
+// This can  bring up error pages if a error occurs with the catch keyword//
 function Result() {
    var num1 = document.getElementById('res').value;
    try {
@@ -11,6 +14,7 @@ function Result() {
       document.getElementById('res').value = 'Error';
    }
 }
+//The function below creates the function of clearing the space in the calcualtor using the id res.//
 function Clear() {
    var inp = document.getElementById('res');
    inp.value = '';
@@ -19,6 +23,8 @@ function Back() {
    var ev = document.getElementById('res');
    ev.value = ev.value.slice(0, -1);
 }
+//The function below uses multiple const variables. It also uses the if conditional statement and the else if multiple times.// 
+// It also helps the numbers and key actions of the calculator run properly.//
 document.addEventListener('keydown', function (event) {
    const key = event.key;
    const validKeys = '0123456789+-*/.%';
